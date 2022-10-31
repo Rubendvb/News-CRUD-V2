5599c0a155e17c71422a510e9a61a993d6293280
Rubendvb <rubenvasquez556@gmail.com>
2022-10-31 14:37:07 -0300

    Ocultando url do mongo

--- server/src/data/dbConfig.ts
+++ server/src/data/dbConfig.ts
@@ -2,12 +2,11 @@ require("dotenv").config();
 import mongoose from "mongoose";
 
 // console.log(process.env.MONGO_URI_PROD);
+const MONGO_URL = process.env.MONGO_URI_PROD;
 
 (async () => {
   try {
-    const db = await mongoose.connect(
-      "mongodb+srv://Rubendvb:iYvMHiOeyJYI6Lyi@apicluster.k1mrpi9.mongodb.net/news?retryWrites=true&w=majority"
-    );
+    const db = await mongoose.connect(MONGO_URL);
     console.log("Mongoose Connect to:", db.connection.name);
   } catch (error) {
     console.error(error);
