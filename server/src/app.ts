ca35c7b88d24fbb050b99a8d1388d25858b4dca2
Rubendvb <rubenvasquez556@gmail.com>
2022-11-08 01:05:51 -0300

    Adicionando caminho no app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from "path";
 
 import NoticiaRoutes from "./routes/news.routes";
 
@@ -10,6 +11,7 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/uploads", express.static(path.resolve("uploads")));
 
 app.use(NoticiaRoutes);
 
